454be72327afeb248c24370a66b730b6155586fa
Nikky Southerland <nikky@uw.edu>
2012-10-11 10:04:11 -0700

    Pull SSH user from system whoami

--- mysql_locator.rb
+++ mysql_locator.rb
@@ -9,7 +9,7 @@ require 'net/ssh'
 
 raise "No user specified. Bailing." if ARGV[0].nil?
 user = ARGV[0].chomp
-
+system_user = `whoami`.chomp
 hosts = [ "ovid01.u.washington.edu",
           "ovid02.u.washington.edu",
           "ovid03.u.washington.edu",
@@ -18,7 +18,7 @@ hosts = [ "ovid01.u.washington.edu",
           ]
 hosts.each do |host|
   fork do
-    Net::SSH.start(host,"nikky") do |ssh|
+    Net::SSH.start(host,system_user) do |ssh|
       output = ssh.exec!("ps -U #{user} -u #{user} u")
       puts "MySQL Match on #{host}" if output =~ /mysql/
     end
