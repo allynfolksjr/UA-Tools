261836ef3d8d048b089f4ae3df5314fbf85fa232
Nikky Southerland <nikky@uw.edu>
2012-10-11 09:58:43 -0700

    Add mysql_locator.rb; renamed repo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-UA-Apps-Installer
+UA Tools
 =================
 
-Automated installer for MySQL and WordPress on Shared UW Hosting. This has not been tested on the uew ovid01..03 systems, and fails to set/use localhome variables and per-user DNS.
+Collect of tools for interacting and using the Uniform Access systems.
---
+++ mysql_locator.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'net/ssh'
+# This is a simple script that will go through various hosts and check to see
+# if a specified user is running MySQLd
+#
+# It assumes the user running the script has ssh keys setup for authentication
+#
+# Requires net-ssh gem and ruby compiled with openssl
+
+raise "No user specified. Bailing." if ARGV[0].nil?
+user = ARGV[0].chomp
+
+hosts = [ "ovid01.u.washington.edu",
+          "ovid02.u.washington.edu",
+          "ovid03.u.washington.edu",
+          "ovid21.u.washington.edu",
+          "vergil.u.washington.edu"
+          ]
+hosts.each do |host|
+  fork do
+    Net::SSH.start(host,"nikky") do |ssh|
+      output = ssh.exec!("ps -U #{user} -u #{user} u")
+      puts "MySQL Match on #{host}" if output =~ /mysql/
+    end
+  end
+  Process.wait
+end
