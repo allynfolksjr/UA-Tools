a82ccb61535c0938e324ccd099d520791df203f7
Nikky Southerland <nikky@uw.edu>
2012-10-12 10:40:05 -0700

    Add check for ovid localhome space; notify if no mysqlds found

--- mysql_locator.rb
+++ mysql_locator.rb
@@ -5,24 +5,54 @@ require 'net/ssh'
 #
 # It assumes the user running the script has ssh keys setup for authentication
 #
-# Requires net-ssh gem and ruby compiled with openssl. Pulls SSH user from 
+# Requires net-ssh gem and ruby compiled with openssl. Pulls SSH user from
 # system's `whoami` command.
 
+
+# Set up initial variables
+$results = 0
 raise "No user specified. Bailing." if ARGV[0].nil?
 user = ARGV[0].chomp
+# get the user of whoever is running script
 system_user = `whoami`.chomp
+# Initialize the systems we'll check
 hosts = [ "ovid01.u.washington.edu",
           "ovid02.u.washington.edu",
           "ovid03.u.washington.edu",
           "ovid21.u.washington.edu",
           "vergil.u.washington.edu"
           ]
-hosts.each do |host|
-  fork do
-    Net::SSH.start(host,system_user) do |ssh|
-      output = ssh.exec!("ps -U #{user} -u #{user} u")
-      puts "MySQL Match on #{host}" if output =~ /mysql/
+
+
+# Checks to see if MySQL is running on a specified host
+def check_for_mysql_presence(host,user,system_user)
+  Net::SSH.start(host,system_user) do |ssh|
+    output = ssh.exec!("ps -U #{user} -u #{user} u")
+    if output =~ /mysql/
+      $results += 1
+      puts "MySQL Match on #{host}"
     end
   end
-  Process.wait
 end
+
+# Returns location of localhome if present
+def check_for_localhome(user,system_user)
+  host = "ovid02.u.washington.edu"
+  Net::SSH.start(host,system_user) do |ssh|
+    output = ssh.exec!("cpw -poh #{user}")
+    if output =~ /Unknown/
+      return "No MySQL Localhome Set for #{user}"
+    else
+      return "Localhome for #{user}: #{output}"
+    end
+  end
+end
+
+
+hosts.each do |host|
+  check_for_mysql_presence(host,user,system_user)
+end
+
+puts "No MySQLds Detected" if $results == 0
+puts "Multiple MySQLds Detected!" if $results > 1
+puts check_for_localhome(user,system_user)
